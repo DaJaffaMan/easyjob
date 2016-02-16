433c1dc62f258fcb196aa0e1eb00fddf7ae659f4
Jack Jefferies <jack-jefferies@live.co.uk>
2016-02-16 14:15:50 +0000

    updating to use java 8 and applying CORS filter to web server

--- easyjob/src/main/java/easyjob/App.java
+++ easyjob/src/main/java/easyjob/App.java
@@ -4,6 +4,7 @@ import easyjob.config.DatabaseConfig;
 import easyjob.handlers.GetAdByAdIdHandler;
 import easyjob.handlers.PostAdHandler;
 import easyjob.transformer.JsonTransformer;
+import filter.CorsFilter;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static spark.Spark.*;
@@ -23,6 +24,8 @@ public class App {
         final String appPort = System.getProperty("app.port", "5000");
         port(Integer.valueOf(appPort));
 
+
+        CorsFilter.apply();
         get("/get/ad/:adId", adByAdIdHandler, transformer);
 
         post("/post/ad/:adId/:email/:adTitle/:adDescription", postAdHandler);
