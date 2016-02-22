394f16bd904798a67b2116d5763417afd8d646cf
Jack Jefferies <jack-jefferies@live.co.uk>
2016-02-22 15:08:46 +0000

    removing spare thing option, adding geo ad repo which will enable ability to find ads by locations

---
+++ easyjob/src/main/java/easyjob/repositories/GeoAdRepository.java
@@ -0,0 +1,30 @@
+package easyjob.repositories;
+
+import easyjob.entities.Ad;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.geo.GeoPoint;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Component
+public class GeoAdRepository {
+
+    private final ElasticsearchTemplate elasticsearchTemplate;
+
+    @Inject
+    public GeoAdRepository(ElasticsearchTemplate elasticsearchTemplate) {
+        this.elasticsearchTemplate = elasticsearchTemplate;
+    }
+
+    public List<Ad> getGeoAd(GeoPoint geoPoint, int range) {
+
+        final CriteriaQuery query = new CriteriaQuery(new Criteria("location").within(geoPoint, range+"km"));
+        final List<Ad> ads = elasticsearchTemplate.queryForList(query, Ad.class);
+
+        return ads;
+    }
+}
