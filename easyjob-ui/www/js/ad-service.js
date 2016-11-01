958a1605a072710fc97af22e049dc22a8e90f39e
jackjefferies <jack-jefferies@live.co.uk>
2016-11-01 20:15:08 +0000

    Formatting and adding initial category functionality

--- easyjob-ui/www/js/ad-service.js
+++ easyjob-ui/www/js/ad-service.js
@@ -1,15 +1,22 @@
 angular.module('starter').service('adService', ['$http', function($http) {
+
+    $defaultUrl = window.location.protocol + '//' + window.location.hostname + ':5000';
+
     return {
         getAdDetailsById: function(adId) {
-            return $http.get(window.location.protocol + '//' + window.location.hostname + ':5000' + '/get/ad/' + adId);
+            return $http.get($defaultUrl + '/get/ad/' + adId);
         },
 
         getAdDetailsByTitle: function(adTitle) {
-            return $http.get(window.location.protocol + '//' + window.location.hostname + ':5000' + '/get/ad/title/' + adTitle);
+            return $http.get($defaultUrl + '/get/ad/title/' + adTitle);
+        },
+
+        getAdDetailsByCategory: function(adCategory) {
+          return $http.get($defaultUrl + '/get/ad/category/' + adCategory)
         },
 
         postAdDetails: function(email, title, description, fee, lat, lon) {
-            return $http.post(window.location.protocol + '//' + window.location.hostname + ':5000' + '/post/ad/' + email + '/' + title + '/' + description + '/' + fee + '/' + lat + '/' + lon);
+            return $http.post($defaultUrl + '/post/ad/' + email + '/' + title + '/' + description + '/' + fee + '/' + lat + '/' + lon);
         }
     };
 }]);
--- easyjob/src/main/java/easyjob/repositories/AdRepository.java
+++ easyjob/src/main/java/easyjob/repositories/AdRepository.java
@@ -1,5 +1,6 @@
 package easyjob.repositories;
 
+import easyjob.categories.Category;
 import easyjob.entities.Ad;
 import org.springframework.data.elasticsearch.repository.ElasticsearchCrudRepository;
 
@@ -8,5 +9,7 @@ import java.util.List;
 public interface AdRepository extends ElasticsearchCrudRepository<Ad, Integer> {
 
     List<Ad> findAdByAdTitleLike(String adTitle);
+
+    List<Ad> findAdByCategory(Category category);
 
 }
