ec8b3b14812621394f62e8ef0c4eee65465bea57
jackjefferies <jack-jefferies@live.co.uk>
2016-11-01 20:30:51 +0000

    formatting and re-adding category controller functionality after merge conflicts

--- easyjob-ui/www/js/ad-service.js
+++ easyjob-ui/www/js/ad-service.js
@@ -1,26 +1,32 @@
-angular.module('starter').service('adService', ['$http', function($http) {
-    return {
-        getAdDetailsById: function(adId) {
-            return $http.get(window.location.protocol + '//' + window.location.hostname + ':5000' + '/get/ad/' + adId);
-        },
+angular.module('starter').service('adService', ['$http', function ($http) {
+  return {
+    getAdDetailsById: function (adId) {
+      return $http.get(window.location.protocol + '//' + window.location.hostname + ':5000' + '/get/ad/' + adId);
+    },
 
-        getAdDetailsByTitle: function(title) {
-            return $http({
-                method: 'GET',
-                url: window.location.protocol + '//' + window.location.hostname + ':5000' + '/get/ad/' + title,
-            });
-        },
+    getAdDetailsByTitle: function (title) {
+      return $http({
+        method: 'GET',
+        url: window.location.protocol + '//' + window.location.hostname + ':5000' + '/get/ad/' + title,
+      });
+    },
 
-        postAdDetails: function(email, title, description, fee, lat, lon) {
+    postAdDetails: function (email, title, description, fee, lat, lon) {
 
-        var ad = {"email": email, "title": title, "description": description, "fee": fee, location: { "lat": lat, "lon": lon}}
+      var ad = {
+        "email": email,
+        "title": title,
+        "description": description,
+        "fee": fee,
+        location: {"lat": lat, "lon": lon}
+      }
 
-            return $http({
-                method: 'POST',
-                url: window.location.protocol + '//' + window.location.hostname + ':5000' + '/post/ad',
-                headers: {"Content-Type": undefined},
-                data: JSON.stringify(ad)
-            });
-        }
-    };
+      return $http({
+        method: 'POST',
+        url: window.location.protocol + '//' + window.location.hostname + ':5000' + '/post/ad',
+        headers: {"Content-Type": undefined},
+        data: JSON.stringify(ad)
+      });
+    }
+  };
 }]);
